8a237756f6219999c4fc92764dfaccda6895aeac
Chaima Alauzet <alzt.chaima@gmail.com>
2023-04-20 08:44:26 +0200

    Ajout des setters

--- exo2.js
+++ exo2.js
@@ -34,4 +34,29 @@ class Avion {
         return this.vitesse;
     }
 
+    // Setters 
+
+    setMarque(new_marque){
+        this.marque = new_marque;
+    }
+
+    setModele(new_modele){
+        this.modele = new_modele;
+    }
+
+    setLongueur(new_longueur){
+        this.longueur = new_longueur;
+    }
+
+    setnbPlace(new_nbPlace){
+        this.nbPlace = new_nbPlace;
+    }
+
+    setReservoir(new_reservoir){
+        this.reservoir = new_reservoir;
+    }
+
+    setVitesse(new_vitesse){
+        this.vitesse = new_vitesse
+    }
 }
